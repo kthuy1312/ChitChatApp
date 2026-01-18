8faba134fd8c1edd571715e7c7e6d40f1c8dfab2
kthuy1312 <lythuy13122004@gmail.com>
2026-01-18 22:15:48 +0700

    chỉnh sửa giao diện SidebarFooter

--- fe/src/components/sidebar/app-sidebar.tsx
+++ fe/src/components/sidebar/app-sidebar.tsx
@@ -24,12 +24,14 @@ import GroupChatList from "../chat/GroupChatList"
 import AddFriendModal from "../chat/AddFriendModal"
 import DirectMessageList from "../chat/DirectMessageList"
 import { useThemeStore } from "@/stores/useThemeStore"
+import { useAuthStore } from "@/stores/useAuthStore"
 
 
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
   const { isDark, toggleTheme } = useThemeStore()
+  const { user } = useAuthStore()
 
   return (
     <Sidebar variant="inset" {...props}>
@@ -95,7 +97,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 
       {/* FOOTER */}
       <SidebarFooter>
-        {/* <NavUser user={data.user} /> */}
+        {user && <NavUser user={user} />}
       </SidebarFooter>
     </Sidebar>
   )
